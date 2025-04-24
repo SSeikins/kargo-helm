c5e9aed9aa91129a07d3f5952831b00f6491a214
Sergejs Sergejs <sergejs.seikins@owboost.eu>
2025-04-24 11:03:58 +0300

    Kargo to Argo

--- download-cli.sh
+++ download-cli.sh
@@ -9,6 +9,7 @@ version=$(basename $(curl -Ls -o /dev/null -w %{url_effective} https://github.co
 os=$(uname -s | tr '[:upper:]' '[:lower:]')
 arch=$(uname -m)
 download_url=https://github.com/akuity/kargo/releases/download/${version}/kargo-${os}-${arch}
+echo $download_url
 
 curl -L -o ${1} ${download_url}
 chmod +x ${1}
